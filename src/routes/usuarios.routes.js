6c0647bf60aed099f44c22021d7cdd8df5fc306f
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-10-31 08:49:18 -0300

    feat: create user class

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -8,8 +8,8 @@ usuariosRoutes.get("/", (req, res) => {
     return res.status(200).json({
         message: 
         usuarios.length == 0 
-        ? "Não há usuários cadastrados" : 
-        `Total de usuários: ${usuarios.length}`,
+        ? "Não há usuários cadastrados" 
+        : `Total de usuários: ${usuarios.length}`,
         usuarios,
     });
 });
