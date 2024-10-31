548a1c7bb40b49ebe979563045756175f9782d6a
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-10-31 09:42:17 -0300

    feat: create post method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -13,11 +13,22 @@ usuariosRoutes.get("/", (req, res) => {
     return res.status(200).json({
         message: 
         usuarios.length == 0 
-        ? "Não há usuários cadastrados" 
+        ? "Não há usuários cadastrados"
         : `Total de usuários: ${usuarios.length}`,
-        usuario
-        }
-    );
+        usuarios,
+        
+    });
+});
+
+usuariosRoutes.post("/", (req, res) => {
+    const {name, email, password} = req.body;
+
+    const usuario = usersRepository.addUser(name, email, password)
+
+    return res.status(201).json({
+        message: "Usuário cadastrado com sucesso!",
+        usuario,
+    });
 });
 
 export default usuariosRoutes;
